f3ace8fa00e1ff240b5a4a54eadcaef4ae1de665
Jens Rasmussen <stamppot@gmail.com>
2010-07-25 17:52:16 +0200

    readme copy javascript

--- public/bcms/bcms_headers/javascripts/random_header.js
+++ public/bcms/bcms_headers/javascripts/random_header.js
@@ -1,7 +1,8 @@
-function random_header(controllerActionString, headerClass, time) {
+function random_header(section_id, headerClass, time) {
+	var urlString = "/random_headers/show/" + section_id.toString();
 	jQuery(headerClass).everyTime(time,function(i){ // div to update
 		jQuery.ajax({
-			url: controllerActionString, /* controller action */
+			url: urlString,
 			cache: false,
 			success: function(header_string){
 				jQuery(headerClass).attr("src").replace(header_string);
