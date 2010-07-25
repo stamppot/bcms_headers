1d3e099059fd292dd2b2518cc079931a146de791
Jens Rasmussen <stamppot@gmail.com>
2010-07-26 00:10:32 +0200

    Added fading to images

--- public/bcms/bcms_headers/javascripts/random_header.js
+++ public/bcms/bcms_headers/javascripts/random_header.js
@@ -1,12 +1,13 @@
 function random_header(page_id, headerClass, time) {
 	var urlString = "/random_headers/show/" + page_id.toString();
 	jQuery(headerClass).everyTime(time,function(i){
+		jQuery(headerClass).animate({opacity: 0.5}, "slow");
 		jQuery.ajax({
 			url: urlString,
 			cache: false,
 			success: function(header_string){
 				jQuery(headerClass).attr("src", header_string);
-			}
-		});
-	});
-}
+				jQuery(headerClass).animate({opacity : 1.0}, "slow");
+				}});
+			});
+		};
